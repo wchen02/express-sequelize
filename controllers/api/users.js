8baae7156c0f446e944624e75a813625cb371d40
Wensheng Chen <hi@wenshengchen.com>
2020-02-02 04:30:43 -0800

    refactor: move route and controller logic to separate files

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,78 +1,22 @@
-const { User } = require('../../models');
+const model = require('../../models').User;
 
-exports.getAll = async (req, res) => {
-  const { rows } = await User.findAndCountAll({
+exports.getAll = async () => {
+  const { rows } = await model.findAndCountAll({
     offset: 0,
     limit: 10,
   });
 
-  res.json(rows);
+  return rows;
 };
 
-exports.get = async (req, res) => {
-  const { id } = req.params;
-  if (!Number.isInteger(Number(id, 10))) {
-    res.sendStatus(400);
-  }
+exports.get = async (id) => model.findByPk(id);
 
-  const user = await User.findByPk(id);
+exports.create = async (data) => model.create(data);
 
-  if (user) {
-    res.json(user);
-  } else {
-    res.sendStatus(404);
-  }
-};
+exports.update = async (id, data) => model.update(data, {
+  where: { id },
+});
 
-exports.create = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-
-    res.json(user);
-  } catch (err) {
-    res.status(400).json({
-      err,
-    });
-  }
-};
-
-exports.update = async (req, res) => {
-  const { id } = req.params;
-  if (!Number.isInteger(Number(id, 10))) {
-    res.sendStatus(400);
-  }
-
-  // destructure id into _ and rest of the body into body
-  const { id: _, ...data } = req.body;
-
-  try {
-    const rows = await User.update(data, {
-      where: { id },
-    });
-
-    res.json({ rows });
-  } catch (err) {
-    res.status(400).json({
-      err,
-    });
-  }
-};
-
-exports.delete = async (req, res) => {
-  const { id } = req.params;
-  if (!Number.isInteger(Number(id, 10))) {
-    res.sendStatus(400);
-  }
-
-  try {
-    const rows = await User.destroy({
-      where: { id },
-    });
-
-    res.json({ rows });
-  } catch (err) {
-    res.status(400).json({
-      err,
-    });
-  }
-};
+exports.delete = async (id) => model.destroy({
+  where: { id },
+});
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,79 @@
 const express = require('express');
-const usersApi = require('../../controllers/api/users');
+const controller = require('../../controllers/api/users');
 
 const router = express.Router();
 
-router.get('/', usersApi.getAll);
-router.get('/:id', usersApi.get);
-router.post('/', usersApi.create);
-router.put('/:id', usersApi.update);
-router.patch('/:id', usersApi.update);
-router.delete('/:id', usersApi.delete);
+router.get('/', async (req, res) => {
+  const rows = await controller.getAll();
+  res.json(rows);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  if (!Number.isInteger(Number(id, 10))) {
+    res.sendStatus(400);
+  }
+
+  const resource = await controller.get(id);
+
+  if (resource) {
+    res.json(resource);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const resource = await controller.create(req.body);
+
+    res.json(resource);
+  } catch (err) {
+    res.status(400).json({
+      err,
+    });
+  }
+});
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  if (!Number.isInteger(Number(id, 10))) {
+    res.sendStatus(400);
+  }
+
+  // destructure id into _ and rest of the body into body
+  const { id: _, ...data } = req.body;
+
+  try {
+    const rows = await controller.update(id, data);
+
+    res.json({ rows });
+  } catch (err) {
+    res.status(400).json({
+      err,
+    });
+  }
+};
+
+router.put('/:id', update);
+
+router.patch('/:id', update);
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  if (!Number.isInteger(Number(id, 10))) {
+    res.sendStatus(400);
+  }
+
+  try {
+    const rows = await controller.destroy(id);
+
+    res.json({ rows });
+  } catch (err) {
+    res.status(400).json({
+      err,
+    });
+  }
+});
 
 module.exports = router;
