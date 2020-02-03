15b1ad2a30c01c53cf44982d0366f9bcc499c162
Wensheng Chen <hi@wenshengchen.com>
2020-02-03 03:33:38 -0800

    refactor: extract  logic into reuable utils

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,6 @@
-const path = require('path');
 const express = require('express');
 const debug = require('debug')('express-sequelize:routes');
-const { getDirFiles } = require('../../utils');
+const { createObjectsFromModels } = require('../../utils');
 const baseApiRoutes = require('./baseApiRoutes');
 const baseControllers = require('../../controllers/api');
 
@@ -11,40 +10,15 @@ router.get('/', (req, res) => {
   res.json({ success: true });
 });
 
-/**
- * Creates routers by scanning models directory and exclude routers
- * that were overriden in the current directory.
- *
- * @example [{ modelName: user, modelApiRouter: router }]
- * @param createRouter callback to create router
- *
- * @returns [{ modelName, modelApiRouter }]
- */
-exports.getModelApiRouters = (createRouter) => {
-  const modelRouterMap = [];
-  const routerFiles = getDirFiles(__dirname, '.js');
-  getDirFiles(
-    path.join(__dirname, '../../models'),
-    '.js',
-    routerFiles,
-    (filename) => {
-      const modelName = path.basename(filename, '.js').toLowerCase();
-      modelRouterMap.push({ modelName, modelApiRouter: createRouter(modelName) });
-    },
-  );
-
-  return modelRouterMap;
-};
-
-const modelApiRouters = exports.getModelApiRouters(
+const modelApiRouters = createObjectsFromModels(
   (modelName) => baseApiRoutes(baseControllers[modelName]),
 );
 
 // eslint-disable-next-line no-restricted-syntax
 for (const modelApiRouterItem of modelApiRouters) {
-  const { modelName, modelApiRouter } = modelApiRouterItem;
-  debug(`Sets up /${modelName} route`);
-  router.use(`/${modelName}`, modelApiRouter);
+  const { name, object } = modelApiRouterItem;
+  debug(`Sets up /${name} route`);
+  router.use(`/${name}`, object);
 }
 
 module.exports = router;
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const fs = require('fs');
 
 exports.getDirFiles = (dir, extension, excludeFiles, callback) => {
@@ -12,4 +13,31 @@ exports.getDirFiles = (dir, extension, excludeFiles, callback) => {
   }
 
   return files;
+};
+
+/**
+ * Creates object by scanning models directory and exclude ones
+ * that were overriden in the current directory.
+ *
+ * @param createObjectCb callback to create object
+ *
+ * @returns [{ name: modelName, object: createdObject }]
+ */
+exports.createObjectsFromModels = (createObjectCb) => {
+  const modelObjectMap = [];
+  const routerFiles = exports.getDirFiles(__dirname, '.js');
+  exports.getDirFiles(
+    path.join(__dirname, '../models'),
+    '.js',
+    routerFiles,
+    (filename) => {
+      const modelName = path.basename(filename, '.js').toLowerCase();
+      modelObjectMap.push({
+        name: modelName,
+        object: createObjectCb(modelName),
+      });
+    },
+  );
+
+  return modelObjectMap;
 };
