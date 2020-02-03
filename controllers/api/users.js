ceef072c4ca8cee814c7af978a8344fb47e7bf04
Wensheng Chen <hi@wenshengchen.com>
2020-02-03 03:32:03 -0800

    chore: remove useless files

--- controllers/api/users.js
+++
@@ -1,5 +0,0 @@
-const baseController = require('./baseController');
-const model = require('../../models').User;
-
-const controller = baseController(model);
-module.exports = controller;
--- routes/api/users.js
+++
@@ -1,5 +0,0 @@
-const baseApiRoutes = require('./baseApiRoutes');
-const controller = require('../../controllers/api/users');
-
-const router = baseApiRoutes(controller);
-module.exports = router;
